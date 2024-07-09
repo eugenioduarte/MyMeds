bc1236b118d83335c49ff38fcfe4d3c233ec191d
Eugenio <eugenio.silva@mindsource.pt>
2024-07-09 21:31:27 +0100

    feat: loading and notification provider is used to show a loading spinner and notifications

---
+++ src/providers/LoadingProvider.tsx
@@ -0,0 +1,87 @@
+import colors from "@/constants/colors";
+import React, { createContext, useContext, useState, ReactNode } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { BlurView } from "expo-blur";
+import LottieView from "lottie-react-native";
+
+type LoadingContextType = {
+  notifyLoading: (flag: boolean) => void;
+};
+
+const LoadingContext = createContext<LoadingContextType | undefined>(undefined);
+
+export const useLoading = () => {
+  const context = useContext(LoadingContext);
+  if (!context) {
+    throw new Error(
+      "useNotification must be used within a NotificationProvider"
+    );
+  }
+  return context;
+};
+
+const NotificationProvider = ({
+  children,
+  isFullScreen,
+}: {
+  children: ReactNode;
+  isFullScreen?: boolean;
+}) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const notifyLoading = (flag: boolean) => {
+    setLoading(flag);
+  };
+// Loading and notification provider is used to show a loading spinner when the app is loading data.
+  return (
+    <LoadingContext.Provider value={{ notifyLoading }}>
+      {children}
+      <View className=" absolute flex-1 h-full w-full items-center justify-center">
+        <BlurView
+          intensity={100}
+          tint="light"
+          className="p-4 items-center justify-center rounded-xl overflow-hidden"
+        >
+          {loading && (
+            <>
+              <LottieView
+                autoPlay
+                loop={false}
+                style={{
+                  width: 200,
+                  height: 200,
+                }}
+                source={require("@assets/animations/pill_animation.json")}
+              />
+              <Text className="p-2 text-white font-bold text-2xl">
+                Loading ...
+              </Text>
+            </>
+          )}
+        </BlurView>
+      </View>
+    </LoadingContext.Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 50,
+    left: 0,
+    right: 0,
+    alignItems: "center",
+    zIndex: 1000,
+  },
+  notification: {
+    backgroundColor: colors.blue_2,
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  error: {
+    backgroundColor: colors.red,
+  },
+});
+
+export default NotificationProvider;
--- src/screens/main/components/CalendarHorizontalScrooll/CalendarHorizontalScroll.tsx
+++ src/screens/main/components/CalendarHorizontalScrooll/CalendarHorizontalScroll.tsx
@@ -1,12 +1,35 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CalendarDayContainer from "./CalendarDayContainer";
+import { format, formatRelative } from "date-fns";
+import { useLoading } from "@/providers/LoadingProvider";
 
 const CalendarHorizontalScroll = () => {
+  const { notifyLoading } = useLoading();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const getRelativeLabel = (date: Date) => {
+    const relativeDate = formatRelative(date, new Date());
+    return (
+      relativeDate.split(" at ")[0].charAt(0).toUpperCase() +
+      relativeDate.split(" at ")[0].slice(1)
+    );
+  };
+
+  useEffect(() => {
+    notifyLoading(true);
+    // setTimeout(() => {
+    //   notifyLoading(false);
+    // }, 2000);
+  }, []);
+
   return (
     <View className="w-full flex items-start justify-start p-2">
-      <Text className="text-grey_2 text-md">January 3</Text>
-      <Text className="text-black text-xl font-semibold">Today reminders</Text>
+      <Text className="text-grey_2 text-md">
+        {format(selectedDate, "MMMM, dd")}
+      </Text>
+      <Text className="text-black text-xl font-semibold">
+        {getRelativeLabel(selectedDate)} reminders
+      </Text>
       <CalendarDayContainer />
     </View>
   );
